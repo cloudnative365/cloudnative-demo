2187a2a99ccd23899643f9b3a2b3d74a897e3699
zhangke0516 <29371962@qq.com>
2019-12-07 18:26:20 +0800

    Update initk8s.sh

--- k8s/initk8s.sh
+++ k8s/initk8s.sh
@@ -6,3 +6,12 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl docker.io
+
+cat <<EOF >>/etc/hosts
+10.0.1.97 ip-10-0-1-97
+10.0.1.18 ip-10-0-1-18
+10.0.1.25 ip-10-0-1-25
+EOF
+
+
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.0.1.97
