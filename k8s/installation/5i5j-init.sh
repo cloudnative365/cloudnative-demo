a2dd5c99bbeb128493542699856aac34f1ae4009
cloudnative365 <29371962@qq.com>
2020-05-14 13:38:06 +0800

    Create 5i5j-init.sh

---
+++ k8s/installation/5i5j-init.sh
@@ -0,0 +1,64 @@
+##########################
+# init environment
+##########################
+# setup kernel
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+##########################
+# Install Runtime
+##########################
+
+# Install Docker CE with the repo from aws
+yum install -y docker
+
+## Create /etc/docker
+mkdir /etc/docker
+
+# Set up the Docker daemon
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart Docker
+systemctl daemon-reload
+systemctl restart docker
+
+
+#####################
+# Install Kubernetes
+#####################
+# Set up the repository:
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+# Disable selinux
+setenforce 0
+
+# install kubeadm
+yum install -y kubelet kubeadm kubectl
+
+# enable kubelet
+systemctl enable kubelet
